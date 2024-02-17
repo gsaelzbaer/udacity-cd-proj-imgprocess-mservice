b492307f71ebfbc6571dfa8cf6e0e27b2152dcde
sneuwirth <codeberg@alpenjodel.de>
2024-02-17 22:45:47 +0100

    (F!!) Change code format/layout

--- project starter code/routes/imageRoutes.js
+++ project starter code/routes/imageRoutes.js
@@ -10,13 +10,16 @@ router.get("/filteredimage", async (req, res) => {
   try {
     let verifiedUrl = new URL(imageUrl)
     filterImageFromURL(verifiedUrl)
-      .then((success) => { res.status(200).send(success); return success })
-      .catch((error) => { res.status(500).send(error.message) })
+      .then((success) => { 
+        res.status(200).send(success); 
+        return success
+      })
       .then((file) => {
         if (file) {
           deleteLocalFiles(new Array(file)) 
         }
-      });
+      })
+      .catch((error) => { res.status(500).send(error.message) });
   } catch (error) {
     return res.status(400).send(error.message);
   }
